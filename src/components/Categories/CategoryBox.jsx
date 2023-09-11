b4ad3f4e7f2aa7bf1bbb41413cbd0888bcac66cf
SadmanJawad <jawadnabil977@gmail.com>
2023-09-11 16:53:51 +0600

    dynamic category and category box done

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+    const updatedQuery = {
+      ...currentQuery,
+      category: label,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    navigate(url);
+  };
+
+  // console.log(value);
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3 border-bottom-2 hover:text-neutral-800 border-transparent text-neutral-500">
+    <div
+      onClick={handleClick}
+      className="flex flex-col cursor-pointer items-center justify-center gap-2 p-3 border-bottom-2 hover:text-neutral-800 border-transparent text-neutral-500"
+    >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
     </div>
